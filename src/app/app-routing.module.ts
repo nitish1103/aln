1a6ed73537e39a7979cf32b5999afaf7c758e4ef
nitish <nitishk82@gmail.com>
2024-04-09 16:24:45 +0530

    Added dashboatrd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
     path: 'create-aln',
     component: CreateAlnComponent,
   },
